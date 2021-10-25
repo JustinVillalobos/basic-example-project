29cb85c0e1c393ac66467e8e3baea8f615bf8111
JustinVillalobos <justin.villalobos@ucr.ac.cr>
2021-10-25 13:38:38 -0600

    Model and Service

--- src/app/pages/all-products/products.module.ts
+++ src/app/pages/all-products/products.module.ts
@@ -5,9 +5,6 @@ import { ProductsRoutingModule } from './products-routing.module';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { InputFormComponent } from 'src/app/shared/components/input-form/input-form.component';
-import { ButtonToggleFormComponent } from 'src/app/shared/components/button-toggle-form/button-toggle-form.component';
-import { HeaderSectionComponent } from 'src/app/shared/components/header-section/header-section.component';
 import { AllProductsComponent } from './all-products.component';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 import { TopBarComponent } from 'src/app/shared/components/top-bar/top-bar.component';
@@ -15,9 +12,6 @@ import { LeftSideBarComponent } from 'src/app/shared/components/left-side-bar/le
 
 @NgModule({
   declarations: [
-  InputFormComponent,
-  ButtonToggleFormComponent,
-  HeaderSectionComponent,
   AllProductsComponent,
   HeaderComponent,
   TopBarComponent,
